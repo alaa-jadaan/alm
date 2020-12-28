67af7f40247322147227d88f452af844a70cf4c5
alaajad01 <alaajad.01@gmail.com>
2020-12-28 22:52:58 +0200

    too much updates
    -إعادة كتابة مكتبة اختيار البلاد والمدن واستخدام البلاد من مكتبة ثانية
    -البلاد والمدن مرنبة أبجديا مع إمكانية البحث
    -تعديل وإضافة بلاد جديدة ومدنها(داغستان, افغانستان, بريطانيا)
    -إضافة الشروط والموافقة عليها حصرا بعد قراءتها اي المرور عليها بسكرول
    -عدم السماح بإدخال 0 بخانة من الجزء
    -change arabic letters regex
    -in submit function: change validate arabic input and check if policies accepted
    -change part field to select

--- js/script.js
+++ js/script.js
@@ -1,77 +1,4 @@
 $(function () {
-    //Sorting options alphabetically and reinitialize sumo select
-    setTimeout(function () {
-        //remove the plugin default placeholder
-        //        $("#nationality-select option[value=''], #country-select option[value='']").remove();
-
-        //        var my_options = $("#nationality-select option");
-        //
-        //        my_options.sort(function (a, b) {
-        //            if (a.text > b.text) return 1;
-        //            if (a.text < b.text) return -1;
-        //            return 0
-        //        });
-
-        //add my placeholder
-        $("#nationality-select option[value=''], #country-select option[value='']").addClass("d-none").text("");
-        //        $("#nationality-select, #country-select").empty().append('<option disabled class="d-none" value=""></option>');
-        //        $("#nationality-select, #country-select").append('<option disabled class="d-none" value=""></option>');
-        //append sorted options
-        //        $("#nationality-select, #country-select").append(my_options);
-        //to select the placeholder option
-        //        var placeholder1 = $("#nationality-select option[value='']");
-        //        var placeholder2 = $("#country-select option[value='']");
-        //        $("#nationality-select").val(placeholder1);
-        //        $("#country-select").val(placeholder2);
-
-        $('#nationality-select')[0].sumo.reload();
-        $('#country-select')[0].sumo.reload();
-    }, 0);
-    //
-    $('.register-page ').on("change", "#country-select", function () {
-        //        //Sorting options alphabetically and reinitialize sumo select
-        //        //remove the plugin default placeholder
-        //        $("#city-select option[value='']").remove();
-        //        var my_options = $("#city-select option");
-        //
-        //        my_options.sort(function (a, b) {
-        //            if (a.text > b.text) return 1;
-        //            if (a.text < b.text) return -1;
-        //            return 0
-        //        });
-        //
-        //        //add my placeholder
-        $("#city-select option[value='']").addClass("d-none").text("");
-        //        $("#city-select").empty().append('<option disabled class="d-none" value=""></option>');
-        //        //append sorted options
-        //        $("#city-select").append(my_options);
-        //        //to select the placeholder option
-        $("#city-select").attr("placeholder", "الرجاء الاختيار");
-        //        var placeholder = $("#city-select option[value='']");
-        //        $("#city-select").val(placeholder);
-        //        
-        $('#city-select')[0].sumo.reload();
-    });
-
-    $('#nationality-select, #country-select').change(function () {
-        //        $("option:selected", this)
-        if ($("option:selected", this).data("class") == "il") {
-            console.log("af");
-            //            $("#country-select").val($("#country-select option").data("class", "ps").val())
-            $("option[data-class='ps']", this).attr("selected", "selected");
-            $(this)[0].sumo.reload();
-        }
-    });
-
-    $('#study-select').change(function () {
-        if ($("#study-select").val() == "4" || $("#study-select").val() == "5") {
-            $("#study-sp-wrapper").removeClass("d-none");
-        } else {
-            $("#study-sp-wrapper").addClass("d-none");
-            $("#study-sp").val("");
-        }
-    });
-
     //    Loading screen
     var ajaxLoadTimeout;
     $(document).ajaxStart(function () {
@@ -84,16 +11,70 @@ $(function () {
         $(".loading-overlay").fadeOut("fast");
     });
 
-    function changeToPartField1() {
+    //display specialization input
+    $('#study-select').change(function () {
+        if ($("#study-select").val() == "4" || $("#study-select").val() == "5") {
+            $("#study-sp-wrapper").removeClass("d-none");
+        } else {
+            $("#study-sp-wrapper").addClass("d-none");
+            $("#study-sp").val("");
+        }
+    });
+    
+//    function changeToPartField1() {
+//        $("#part-error").addClass("d-none");
+//        $("#to-part-text").removeClass("d-none");
+////        $("#from-part").vl("1");
+//        $("#from-part").val("");
+//        if ($("#subject-select").find('option:selected').data("multi")) {
+//            $("#part-wrapper").removeClass("d-none");
+//            $("#from-part").attr("required", "required");
+//            var partsNum = Number($("#subject-select").find('option:selected').data("count"));
+//            var sum = Number($("#from-part").val()) + partsNum - 1;
+//            if (sum > 30) {
+//                $("#to-part").html("");
+//                $("#part-error").removeClass("d-none");
+//                $("#to-part-text").addClass("d-none");
+//            } else {
+//                $("#to-part").html(sum);
+//            }
+//
+//        } else {
+//            $("#from-part").removeAttr("required");
+////            $("#from-part").val("1");
+//            $("#from-part").val("");
+//            $("#to-part").html("");
+//            $("#part-error").addClass("d-none");
+//            $("#to-part-text").removeClass("d-none");
+//            $("#part-wrapper").addClass("d-none");
+//        }
+//    }
+//
+//    function changeToPartField() {
+//        $("#part-error").addClass("d-none");
+//        $("#to-part-text").removeClass("d-none");
+//        var partsNum = Number($("#subject-select").find('option:selected').data("count"));
+//        var sum = Number($("#from-part").val()) + partsNum - 1;
+//        if (sum > 30) {
+//            $("#to-part").html("");
+//            $("#part-error").removeClass("d-none");
+//            $("#to-part-text").addClass("d-none");
+//        } else {
+//            $("#to-part").html(sum);
+//        }
+//    }
+
+    function subjectChange() {
+        console.log($('#from-part-select').val());
         $("#part-error").addClass("d-none");
         $("#to-part-text").removeClass("d-none");
-        $("#from-part").val("1");
+        $('#from-part-select')[0].sumo.unSelectAll();
+        $("#from-part-select option:selected").prop("selected", false)
         if ($("#subject-select").find('option:selected').data("multi")) {
             $("#part-wrapper").removeClass("d-none");
-            $("#from-part").attr("required", "required");
+            $("#from-part-select").attr("required", "required");
             var partsNum = Number($("#subject-select").find('option:selected').data("count"));
-            var sum = Number($("#from-part").val()) + partsNum - 1;
-            console.log(sum)
+            var sum = Number($("#from-part-select").val()) + partsNum - 1;
             if (sum > 30) {
                 $("#to-part").html("");
                 $("#part-error").removeClass("d-none");
@@ -103,8 +84,8 @@ $(function () {
             }
 
         } else {
-            $("#from-part").removeAttr("required");
-            $("#from-part").val("1");
+            $("#from-part-select").removeAttr("required");
+            $("#from-part").val("");
             $("#to-part").html("");
             $("#part-error").addClass("d-none");
             $("#to-part-text").removeClass("d-none");
@@ -112,12 +93,11 @@ $(function () {
         }
     }
 
-    function changeToPartField() {
+    function partChange() {
         $("#part-error").addClass("d-none");
         $("#to-part-text").removeClass("d-none");
         var partsNum = Number($("#subject-select").find('option:selected').data("count"));
-        var sum = Number($("#from-part").val()) + partsNum - 1;
-        console.log(sum)
+        var sum = Number($("#from-part-select").val()) + partsNum - 1;
         if (sum > 30) {
             $("#to-part").html("");
             $("#part-error").removeClass("d-none");
@@ -126,19 +106,40 @@ $(function () {
             $("#to-part").html(sum);
         }
     }
-
+    
     $('#subject-select').change(function () {
-        changeToPartField1();
+        subjectChange();
     });
 
-    $("#from-part").on('keyup paste', function (event) {
-        changeToPartField();
+    //add from-part-select
+    for (j = 1; j <= 28; j += 1) {
+        $("#from-part-select").append($('<option>', {
+            value: j,
+            text: j
+        }));
+    }
+    $('#from-part-select')[0].sumo.reload();
+    
+    $('#from-part-select').change(function () {
+        partChange();
     });
+//    $("#from-part").on('keyup paste', function (event) {
+//        changeToPartField();
+//    });
+    
+//    $("#from-part").on('focusout', function (event) {
+//        if($(this).val() == 0){
+//            $("#to-part").html("");
+//            $("#part-error").removeClass("d-none");
+//            $("#to-part-text").addClass("d-none");
+//        }
+//    });
 
     //arabic letters validation
     function onlyArabic($field) {
         // Arabic characters fall in the Unicode range 0600 - 06FF
-        var arabicCharUnicodeRange = /[\u0600-\u06FF]/;
+//        var arabicCharUnicodeRange = /[\u0600-\u06FF]/;
+                var arabicCharUnicodeRange = /[ء-ي]/;
         //        var arabicCharUnicodeRange = /[ء-ي]/;
         //        var arabicCharUnicodeRange = /([\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\ufdf0-\ufdfd])/;
         //        var regex = new RegExp("[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufc3f]|[\ufe70-\ufefc]");
@@ -209,27 +210,26 @@ $(function () {
     onlyNumbers($("#phone-confirm"));
 
     //allow only numbers between 1-30
-    $("#from-part").on('keypress', function (event) {
-        var range = /^[0-9]+$/;
-        var key = event.which;
-        // 0 = numpad
-        // 8 = backspace
-        if (key == 8 || key == 0) {
-            return true;
-        }
-
-        var str = String.fromCharCode(key);
-        if (range.test(str)) {
-            var newVal = $(this).val() + event.key;
-            console.log(newVal);
-            if (Number(newVal) > 30) {
-                return false;
-            }
-            return true;
-        }
-        return false;
-    });
-
+//    $("#from-part").on('keypress paste', function (event) {
+//        var range = /^[0-9]+$/;
+//        var key = event.which;
+//        // 0 = numpad
+//        // 8 = backspace
+//        if (key == 8 || key == 0) {
+//            return true;
+//        }
+//
+//        var str = String.fromCharCode(key);
+//        if (range.test(str)) {
+//            var newVal = $(this).val() + event.key;
+//            if (Number(newVal) > 30) {
+//                return false;
+//            }
+//            return true;
+//        }
+//        return false;
+//    });
+    
     function checkPhoneMatch() {
         var phone = $("#phone").val();
         var confirmPhone = $("#phone-confirm").val();
@@ -256,24 +256,7 @@ $(function () {
     $("#phone").keyup(checkPhoneMatch);
     $("#phone-confirm").keyup(checkPhoneMatch);
 
-    //get quran subjects
-    $.ajax({
-        type: "get",
-        url: "http://pure-journey-56274.herokuapp.com/api/quran_subjects",
-        dataType: "json"
-    }).done(function (data) {
-        var i;
-        for (i in data) {
-            $('#subject-select').append($('<option>', {
-                value: data[i].id,
-                text: data[i].name,
-                "data-multi": data[i].multi,
-                "data-count": data[i].count
-            }));
-        }
-        $('#subject-select')[0].sumo.reload();
-    });
-
+    //remove invalid style when select option
     $("select.form-control").change(function () {
         if ($(this).siblings(".CaptionCont").hasClass("sumo-invalid")) {
             $(this).siblings(".CaptionCont").removeClass("sumo-invalid")
@@ -302,13 +285,15 @@ $(function () {
                 return false;
             } else {
                 //validate arabic input
-                var arabicCharUnicodeRange = /[\u0600-\u06FF]/;
+                var arabicCharUnicodeRange = /[ء-ي-\s]/;//arabic and space
 
                 var first_name = $("#first_name").val();
                 var father_name = $("#father_name").val();
                 var last_name = $("#last_name").val();
 
-                if (!arabicCharUnicodeRange.test(first_name) || !arabicCharUnicodeRange.test(father_name) || !arabicCharUnicodeRange.test(last_name)) {
+                if (!((first_name.split(arabicCharUnicodeRange).length - 1) == first_name.length) ||
+                    !((father_name.split(arabicCharUnicodeRange).length - 1) == father_name.length) ||
+                    !((last_name.split(arabicCharUnicodeRange).length - 1) == last_name.length)) {
                     $("html, body").animate({
                         scrollTop: $("form").offset().top
                     }, "slow");
@@ -317,12 +302,17 @@ $(function () {
                     $("#error-message").removeClass("d-none");
                     return false;
                 }
+                
                 //validate phone
                 if (!($("#error-msg").hasClass("d-none"))) {
+                    $("html, body").animate({
+                        scrollTop: $("form").offset().top
+                    }, "slow");
                     $("#error-message").html("*الرجاء كتابة رقم الوتساب بشكل صحيح");
                     $("#error-message").removeClass("d-none");
                     return false;
                 }
+                
                 //validate phone and phone-confirm match
                 var phone = $("#phone").val();
                 var confirmPhone = $("#phone-confirm").val();
@@ -341,7 +331,7 @@ $(function () {
                 if ($("#study-select").val() == "4" || $("#study-select").val() == "5") {
                     var specialization = $("#study-sp").val();
                     if (specialization != "") {
-                        if (!arabicCharUnicodeRange.test(specialization)) {
+                        if (!((specialization.split(arabicCharUnicodeRange).length - 1) == specialization.length)) {
                             $("html, body").animate({
                                 scrollTop: $("form").offset().top
                             }, "slow");
@@ -352,7 +342,57 @@ $(function () {
                     }
 
                 }
-
+                
+                //validate parts
+//                var numberUnicodeRange = /[1-9]/;
+//                var fromPart = $("#from-part").val();
+//                if (fromPart != "") {
+//                    if (!((fromPart.split(numberUnicodeRange).length - 1) == fromPart.length)) {
+//                        $("html, body").animate({
+//                            scrollTop: $("form").offset().top
+//                        }, "slow");
+//                        $("#error-message").html("*الرجاء كتابة رقم الجزء بالإنكليزية");
+//                        $("#error-message").removeClass("d-none");
+//                        return false;
+//                        }
+//                    }
+//
+//                    if($("#from-part").val() === 0){
+//                        $("#to-part").html("");
+//                        $("#part-error").removeClass("d-none");
+//                        $("#to-part-text").addClass("d-none");
+//                        
+//                        $("html, body").animate({
+//                            scrollTop: $("form").offset().top
+//                        }, "slow");
+//                        $("#error-message").html("*الرجاء إدخال قيمة صحيحة في حقل من الجزء");
+//                        $("#error-message").removeClass("d-none");
+//                        return false;
+//                    }
+//                
+//                if ($("#from-part").val() > 30){
+//                    $("#to-part").html("");
+//                    $("#part-error").removeClass("d-none");
+//                    $("#to-part-text").addClass("d-none");
+//
+//                    $("html, body").animate({
+//                        scrollTop: $("form").offset().top
+//                    }, "slow");
+//                    $("#error-message").html("*الرجاء إدخال قيمة صحيحة في حقل من الجزء");
+//                    $("#error-message").removeClass("d-none");
+//                    return false;
+//                }
+                
+                //check if policies accepted
+                if(!($("#accept").is(':checked'))){
+                    $("html, body").animate({
+                        scrollTop: $("form").offset().top
+                    }, "slow");
+                    $("#error-message").html("*الرجاء قراءة شروط التسجيل والموافقة عليها");
+                    $("#error-message").removeClass("d-none");
+                    return false;
+                }
+                
                 var parts;
                 data = $(this).serializeArray();
                 if ($("#subject-select").find('option:selected').data("multi")) {
@@ -362,8 +402,6 @@ $(function () {
                         value: parts
                     });
                 }
-
-                console.log(data);
 
                 $.ajax({
                     type: "post",
@@ -380,9 +418,6 @@ $(function () {
         }
     });
 
-    //    $('#error-modal').modal("show")
-    //    $('#success-modal').modal("show")
-
     //initialize tooltips
     $('[data-toggle="tooltip"]').tooltip();
 
@@ -397,5 +432,11 @@ $(function () {
     }
     $('#birthday-select')[0].sumo.reload();
 
+    //check if policies are read
+    $('#policies').on('scroll', function() {
+    if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+        $("#accept").removeAttr("disabled");
+        }
+    });
 
 });
